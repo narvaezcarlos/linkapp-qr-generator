3ed21f0fe26c9c23c3ba2b53100e83af189f04c5
narvaezcarlos <narvaezcarlos@live.com>
2023-11-10 14:22:22 -0500

    Correccion qr generator

--- react/components/Landing/index.tsx
+++ react/components/Landing/index.tsx
@@ -15,7 +15,7 @@ const Landing = () => {
         const params = new URLSearchParams(location.search);
         const id = params.get('id');
         const src = params.get('src');
-        setUrl(encodeURIComponent(`/tienda?id=${id}&src=${src}`));
+        setUrl(encodeURIComponent(`https://ventadirectanewqa.myvtex.com/tienda?id=${id}&src=${src}`));
     }
 
     useEffect(() => {
