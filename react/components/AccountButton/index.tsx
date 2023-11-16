9cc9b733ae146003f4a7665ff6944589a881499c
jpma-dev <juanpmesa99@gmail.com>
2023-11-16 11:04:03 -0500

    Fix app landing qr

--- react/components/AccountButton/index.tsx
+++ react/components/AccountButton/index.tsx
@@ -10,7 +10,7 @@ const AccountButton = () => {
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
             </svg>
-            Mi Cuenta
+            Ir a mis ganancias
             <svg fill="none" width="16" height="16" viewBox="0 0 100 100" className={accountLoading}>
                 <g id="sti-loading">
                     <circle cx="50" opacity="0.4" cy="50" fill="none" stroke="#000000" r="40" className="c-muted-1" stroke-width="14"></circle>
